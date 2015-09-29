33b6d4424307b28a3a3b8a19f62cf3b21e0936a7
Juan David Vega <jdvr.93@hotmail.com>
2015-09-29 23:44:35 +0200

    using pipe  to print data on the end of the transaction

---
+++ httpClientPipe.js
@@ -0,0 +1,16 @@
+var bl = require('bl');
+var http = require('http');
+
+var url = process.argv[2];
+
+http.get(url, function (response) {
+  response.on('error', console.error);
+  response.pipe(bl(function (err, data) {
+    if(err) return console.error(err);
+
+    console.log(data.toString().length);
+
+    console.log(data.toString());
+
+  }))
+});
